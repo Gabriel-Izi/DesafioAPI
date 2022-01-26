8e74ee989891778753e1f9d370fdff3389e376cf
Gabriel-Izi <gabriel.ios89@hotmail.com>
2022-01-26 13:47:08 -0300

    Alterações no pom e no run test

--- src/test/java/api/ApiBody.java
+++ src/test/java/api/ApiBody.java
@@ -94,11 +94,7 @@ public class ApiBody {
                 .userId(1)
                 .id(1)
                 .title("sunt aut facere repellat provident occaecati excepturi optio reprehenderit")
-                .body("""
-                        quia et suscipit
-                        suscipit recusandae consequuntur expedita et cum
-                        reprehenderit molestiae ut ut quas totam
-                        nostrum rerum est autem sunt rem eveniet architecto""")
+                .body("quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto")
                 .build();
 
     }
